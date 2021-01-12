0f4d608f81be1448fc91690cf123051fffccc996
peng49 <1219955253@qq.com>
2021-01-12 13:34:58 +0800

    增加 'open Feign' 依赖并配置接口

--- eureka-demo/eureka-consumer/src/main/java/com/github/peng49/springclouddemo/EurekaConsumerApplication.java
+++ eureka-demo/eureka-consumer/src/main/java/com/github/peng49/springclouddemo/EurekaConsumerApplication.java
@@ -1,8 +1,10 @@
 package com.github.peng49.springclouddemo;
 
+import com.github.peng49.springclouddemo.service.TestService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -11,13 +13,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
 import javax.annotation.Resource;
 
 @SpringBootApplication
 @RestController
+@EnableFeignClients
 public class EurekaConsumerApplication {
     @Resource
     private RestTemplate restTemplate;
+
+    @Resource
+    private TestService testService;
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaConsumerApplication.class, args);
@@ -36,5 +44,10 @@ public class EurekaConsumerApplication {
         System.out.println(responseEntity.getBody());
 
         return responseEntity.getBody();
+    }
+
+    @RequestMapping("/consumerForFeign/{id}")
+    public Map<String, String> consumerForFeignTest(@PathVariable("id") Integer id) {
+        return testService.providerMethod(id);
     }
 }
