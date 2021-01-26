3320d3ba4e1a66a1177a47187c0b6e064fe5803d
peng49 <1219955253@qq.com>
2021-01-26 14:56:44 +0800

    test sentinel

---
+++ springcloudalibaba-sentinel/src/test/java/com/github/peng49/springclouddemo/sentinel/SentinelTest.java
@@ -0,0 +1,57 @@
+package com.github.peng49.springclouddemo.sentinel;
+
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SentinelTest {
+
+    @Test
+    public void singleExceptionTest()
+    {
+        List<FlowRule> rules = new ArrayList<>();
+
+        FlowRule rule = new FlowRule();
+
+        rule.setResource("tutorial");
+        // QPS 不得超出 1
+        rule.setCount(1);
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        rule.setLimitApp("default");
+
+        rules.add(rule);
+
+        // 加载规则
+        FlowRuleManager.loadRules(rules);
+
+
+        // 下面开始运行被限流作用域保护的代码
+        while (true) {
+            Entry entry = null;
+            try {
+                entry = SphU.entry("tutorial");
+                System.out.println("hello world");
+            } catch (BlockException e) {
+                System.out.println("blocked");
+            } finally {
+                if (entry != null) {
+                    entry.exit();
+                }
+            }
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+
+            }
+        }
+    }
+
+
+}
